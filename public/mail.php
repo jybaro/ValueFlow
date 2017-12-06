7726ba1f7ba2affdcad4cc75e4da7b451ef175fa
Edgar Valarezo <edgar.valarezo@gmail.com>
2017-12-06 17:24:51 -0500

    ejemplo de mail

--- public/mail.php
+++ public/mail.php
@@ -1,10 +1,22 @@
 <?php
 
-require_once('../library/PHPMailer_v5.1/class.phpmailer.php');
+//require_once('../library/PHPMailer_v5.1/class.phpmailer.php');
+//require_once('vendor/phpmailer/phpmailer/src/PHPMailer.php');
+require_once('vendor/autoload.php');
+
+define('SMTP_SERVER', 'mail.nedetel.net');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'sait@nedetel.net');
+define('SMTP_PASSWORD', 'n3D1$207*');
+
+define('MAIL_ORDERS_ADDRESS', 'sait@nedetel.net');
+define('MAIL_ORDERS_NAME', 'SAIT');
+
 
         try{
-            $mail = new PHPMailer(true);
+            $mail = new PHPMailer\PHPMailer\PHPMailer(true);
             $mail->IsSMTP();
+            $mail->SMTPSecure = 'tls';
             $mail->SMTPAuth = true;
             $mail->Host = SMTP_SERVER;
             $mail->Port = SMTP_PORT;
@@ -12,9 +24,13 @@ require_once('../library/PHPMailer_v5.1/class.phpmailer.php');
             $mail->Password = SMTP_PASSWORD;
             $mail->SMTPDebug = 2;
             $mail->SetFrom(MAIL_ORDERS_ADDRESS, MAIL_ORDERS_NAME);
-            $mail->Subject = 'AMAZONEK.cz - objednávka číslo '.$_SESSION['orderId'];
-            $mail->MsgHTML('<b>Ahoj</b>');
-            $mail->AddAddress($_SESSION['user']['email'], $_SESSION['user']['name'].' '.$_SESSION['user']['surname']);
+            $mail->Subject = 'prueba';
+            $mail->MsgHTML('<b>Esto es una prueba desde SAIT</b>');
+            $mail->AddAddress('edgar.valarezo@gmail.com');
+            //$mail->AddAddress('sminga@nedetel.net');
+            //$mail->AddAddress('dcedeno@nedetel.net');
+            $mail->AddAttachment('prueba.txt');
+            $mail->AddAttachment('prueba.pdf');
             $mail->AddBCC(MAIL_ORDERS_ADDRESS, MAIL_ORDERS_NAME);
 
             if(!$mail->Send()) throw new Exception($mail->ErrorInfo);
--- public/vendor/composer/autoload_static.php
+++ public/vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitab185a8445c5417341271d5ce3ace61b
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'H' => 
         array (
@@ -19,6 +33,8 @@ class ComposerStaticInitab185a8445c5417341271d5ce3ace61b
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitab185a8445c5417341271d5ce3ace61b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitab185a8445c5417341271d5ce3ace61b::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitab185a8445c5417341271d5ce3ace61b::$prefixesPsr0;
 
         }, null, ClassLoader::class);
