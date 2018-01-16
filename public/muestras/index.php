02b1d6e59357ce2a5a22d8198666c2de19cf6ce3
root <root@localhost.localdomain>
2018-01-16 00:27:57 -0500

    Agregadas las 9 muestras

---
+++ public/muestras/index.php
@@ -0,0 +1,12 @@
+
+<ul>
+<li><a href="/muestras/cerulean/public/">Muestra 1</a></li>
+<li><a href="/muestras/cosmo/public/">Muestra 2</a></li>
+<li><a href="/muestras/darkly/public/">Muestra 3</a></li>
+<li><a href="/muestras/flaty/public/">Muestra 4</a></li>
+<li><a href="/muestras/simplex/public/">Muestra 5</a></li>
+<li><a href="/muestras/slate/public/">Muestra 6</a></li>
+<li><a href="/muestras/spacelab/public/">Muestra 7</a></li>
+<li><a href="/muestras/superhero/public/">Muestra 8</a></li>
+<li><a href="/muestras/united/public/">Muestra 9</a></li>
+</ul>
