11d33181579185202c1d075d7a6a5022f215eb05
Edgar Valarezo <edgar.valarezo@gmail.com>
2018-02-20 16:36:19 -0500

    valores historicos de los campos

--- public/proceso.php
+++ public/proceso.php
@@ -1841,8 +1841,8 @@ function p_desplegar_campos(campos, padre_id) {
 
     campos.forEach(function(campo){
         var valor_historico = (campo['valor_historico'] == 'null' || campo['valor_historico'] == null) ? '' : campo['valor_historico'];
-        var valor_por_defecto = (campo['valor_por_defecto'] == 'null' || campo['valor_por_defecto'] == null) ? valor_historico : campo['valor_por_defecto'];
-        var valor = (campo['valor'] == 'null' || campo['valor'] == null) ? valor_por_defecto : campo['valor'];
+        var valor_por_defecto = (campo['valor_por_defecto'] == 'null' || campo['valor_por_defecto'] == null  || campo['valor_por_defecto'] == '') ? valor_historico : campo['valor_por_defecto'];
+        var valor = (campo['valor'] == 'null' || campo['valor'] == null || campo['valor'] == '') ? valor_por_defecto : campo['valor'];
 
         var contenido = '';
         console.log('CAMPO:', campo);
