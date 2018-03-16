44a3d59e800b414e3a994c0eab3b666601981162
Edgar Valarezo <edgar.valarezo@gmail.com>
2018-03-16 15:15:36 -0500

    ticket 164: agregada nueva clase que ajusta el alto al texto del campo, en la ventana de detalle del punto, en proceso.php

--- public/proceso.php
+++ public/proceso.php
@@ -861,9 +861,15 @@ foreach($provincias as $provincia) {
 </div>
 
 
+<style>
+.autoalto .form-control{
+height:auto;
+min-height: 30px;
+overflow: hidden;
+}
+</style>
 
-
-<div id="modal_detalle_nodo" class="modal fade" tabindex="-1" role="dialog">
+<div id="modal_detalle_nodo" class="modal fade autoalto" tabindex="-1" role="dialog">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
