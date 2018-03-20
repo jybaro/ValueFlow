f57bba5b72d03ecca4a36439cb7f512c71a41deb
Edgar Valarezo <edgar.valarezo@gmail.com>
2018-03-20 15:56:18 -0500

    ticket 171: quitado ID_SERVICIO de plantillas de mensajes y puesto ID_ORDEN_SERVICIO

--- public/actualizacion_bdd.php
+++ public/actualizacion_bdd.php
@@ -1,11 +1,28 @@
 <?php
 
+echo "<pre>";
 $result = q("
 UPDATE sai_archivo SET 
 arc_nombre = replace(arc_nombre, 'ID_SERVICIO', 'ID_ORDEN_SERVICIO') 
 ,arc_ruta = replace(arc_ruta, 'ID_SERVICIO', 'ID_ORDEN_SERVICIO') 
-WHERE arc_nombre ILIKE '%ID_SERVICIO%'
+WHERE arc_borrado IS NULL
+AND arc_nombre LIKE '%ID\_SERVICIO%'
 RETURNING *
 ");
-echo "<pre>";
+var_dump($result);
+
+
+
+$result = q("
+UPDATE sai_plantilla
+SET
+pla_asunto = replace(pla_asunto, 'ID_SERVICIO', 'ID_ORDEN_SERVICIO')
+,pla_cuerpo = replace(pla_cuerpo, 'ID_SERVICIO', 'ID_ORDEN_SERVICIO')
+WHERE pla_borrado IS NULL 
+AND (
+pla_asunto LIKE '%ID\_SERVICIO%'
+OR pla_cuerpo LIKE '%ID\_SERVICIO%'
+)
+RETURNING *
+");
 var_dump($result);
